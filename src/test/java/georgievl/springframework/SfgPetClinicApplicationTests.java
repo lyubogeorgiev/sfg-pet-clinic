3197bc79698394d9d5ea56dbee8fa33419939c4d
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-05-19 11:44:54 -0600

    Initial Project Add

---
+++ src/test/java/georgievl/springframework/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package georgievl.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
